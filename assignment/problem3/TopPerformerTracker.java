0f30213e53cd881f3cd249cbaef97d0feb93d220
Sarvagya-dot <sg2709@srmist.edu.in>
2026-09-12 14:52:08 +0530

    Add Week5 Assignment Problem3: Top Performer Tracker

---
+++ assignment/problem3/TopPerformerTracker.java
@@ -0,0 +1,24 @@
+public class TopPerformerTracker {
+
+    public static void main(String[] args) {
+        int[] sampleScores = {45, 82, 79, 90, 33, 90, 61};
+        System.out.println(findMinMaxSpread(sampleScores));
+    }
+
+    static String findMinMaxSpread(int[] scores) {
+        int minimumScore = scores[0];
+        int maximumScore = scores[0];
+
+        for (int index = 1; index < scores.length; index++) {
+            if (scores[index] < minimumScore) {
+                minimumScore = scores[index];
+            }
+            if (scores[index] > maximumScore) {
+                maximumScore = scores[index];
+            }
+        }
+
+        int spread = maximumScore - minimumScore;
+        return "Min: " + minimumScore + " | Max: " + maximumScore + " | Spread: " + spread;
+    }
+}
